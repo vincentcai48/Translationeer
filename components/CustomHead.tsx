af4e271a70717807668ea12140d2a8d686edbcc6
vincentc48 <vincent.cai48@gmail.com>
2021-07-02 12:00:16 -0400

    basic files 1

---
+++ components/CustomHead.tsx
@@ -0,0 +1,7 @@
+import Head from "next/head"
+
+export default function CustomHead(){
+    return <Head>
+        <title>Translationeer</title>
+    </Head>
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import Header from '../components/Header'
+import CustomHead from "../components/CustomHead"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <CustomHead></CustomHead>
+    <Header></Header>
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
