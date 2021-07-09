4100fa3bbdf0b48959bc31dd4b65bcee1b74978f
vincentc48 <vincent.cai48@gmail.com>
2021-07-08 21:58:44 -0400

    add legal folder

---
+++ pages/legal/disclaimers.js
@@ -0,0 +1,6 @@
+import Comp from "../../components/legal/Disclaimers";
+import React from "react";
+
+export default function Container() {
+  return <Comp />;
+}
---
+++ pages/legal/termsandconditions.js
@@ -0,0 +1,6 @@
+import Comp from "../../components/legal/TermsAndConditions";
+import React from "react";
+
+export default function Container() {
+  return <Comp />;
+}
